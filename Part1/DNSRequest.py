0a9eb2e0959ccdd8f45adb4a8d3d5bc273f4754e
FunBunyan <austinb1632@gmail.com>
2023-11-17 16:21:25 -0800

    Draft, everything done but print statements are still included

--- Part1/DNSRequest.py
+++ Part1/DNSRequest.py
@@ -20,12 +20,12 @@ def PacketConstruion(domain):
     question = b''
 
     for part in domain.split("."):
-        question += (len(part).to_bytes(1, 'big')) + part.encode("utf-8")
+        question += (len(part).to_bytes(1, 'big')) + part.encode()
     question += struct.pack("!B", 0) + struct.pack("!HH", 1, 1)
 
     # print(header, "Question is", question)
     request = header + question
-    print("Total string is", request)
+    # print("Total string is", request)
     return request
 
 def recieveDNSResponse(socket):
@@ -50,19 +50,18 @@ def recieveDNSResponse(socket):
     # parseNameOfNameServers is tuple, which is needed to unpack
     parseNameOfNameServers = ("1")
     for answer in range(totalRR):
-        # parseNameOfNameServers = ("1")
         parseNameOfNameServers = struct.unpack("!B", recieve[recieveCounter : recieveCounter + 1])
-        print((parseNameOfNameServers[0]))
+        # print((parseNameOfNameServers[0]))
         
         # Check if first two bits in string are 11 using int (11000000 == 192)
         if (parseNameOfNameServers[0] >= 192):
             recieveCounter += 2
+        # Name of domain will be null terminated if full url is used
         else:
             while(parseNameOfNameServers[0] != 0):
                 parseNameOfNameServers = struct.unpack("!B", recieve[recieveCounter : recieveCounter + 1])
                 # print("Name servers:", parseNameOfNameservers)
                 recieveCounter += 1
-            recieveCounter -= 1 
         
         dnsType, dnsClass = struct.unpack("!HH", recieve[recieveCounter : recieveCounter + 4])
         # print("DNS type and class:", dnsType, dnsClass)
@@ -88,18 +87,55 @@ def recieveDNSResponse(socket):
         else:
             recieveCounter += dnsDataLength[0]
         # print("Iterator at ", answer + 1)
-            
+
+def send_http_request_tcp_by_ip(ip, port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcpSocket:
+        tcpSocket.connect((ip, port))
+        tcpSocket.settimeout(3)
+
+        request = f"GET / HTTP/1.1\r\nHost: {ip}\r\n\r\n"
+        
+        tcpSocket.sendall(request.encode())
+
+        response = ""
+        try:
+            while True:
+                data = tcpSocket.recv(1024).decode()
+                if not data:
+                    tcpSocket.close()
+                    break
+                response += data
+        except socket.timeout:
+            pass
+
+        return response
+    
+def main():
+    udpSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    message = PacketConstruion(WEBSITE)
+    udpSocket.sendto(message, (ROOTIP, DNSPORT))
+
+    tldIP = recieveDNSResponse(udpSocket)
+    udpSocket.sendto(message, (tldIP, DNSPORT))
+
+    authoratativeIP = recieveDNSResponse(udpSocket)
+    udpSocket.sendto(message, (authoratativeIP, DNSPORT))
+
+    ip = recieveDNSResponse(udpSocket)
+    print(WEBSITE, "IP is", ip)
+
+    response = send_http_request_tcp_by_ip(ip, 80)
+    
+    print(response)
+
+    # Uncomment to export to file
+    # with open("Response.txt", "w") as response_file:
+    #     response_file.write(response)
+    
+if __name__ == "__main__":
+    main()
     
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-message = PacketConstruion(WEBSITE)
-sock.sendto(message, (ROOTIP, DNSPORT))
 
-tldIP = recieveDNSResponse(sock)
-sock.sendto(message, (tldIP, DNSPORT))
 
-authoratativeIP = recieveDNSResponse(sock)
-sock.sendto(message, (authoratativeIP, DNSPORT))
 
-ip = recieveDNSResponse(sock)
-print(ip)
