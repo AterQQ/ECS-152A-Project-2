540452c4b8b806aa18f5ed1b90f7453384a4adf8
FunBunyan <austinb1632@gmail.com>
2023-11-19 00:11:11 -0800

    Final implementation done for part 1

--- Part1/DNSRequest.py
+++ Part1/DNSRequest.py
@@ -2,12 +2,19 @@ import socket
 import random
 import struct
 import time
+import copy
+
+# Sources:
+# https://www.rfc-editor.org/rfc/rfc1035 (for figuring out DNS bytes)
+# Harron's ECS 152 Github Page: https://github.com/Haroon96/ecs152a-fall-2023
 
 ROOTIP = "199.9.14.201"
 DNSPORT = 53
+HTTPPORT = 80
 WEBSITE = "tmz.com"
 
 def PacketConstruion(domain):
+    # Header construction
     id = random.randint(1, 65536)
     flags = 0
     numQuestions = 1
@@ -17,21 +24,22 @@ def PacketConstruion(domain):
 
     header = struct.pack("!HHHHHH", id, flags, numQuestions, numAnswerRR, numAuthorityRR, numAdditionalRR)
 
+    # Query Construction
     question = b''
 
     for part in domain.split("."):
         question += (len(part).to_bytes(1, 'big')) + part.encode()
     question += struct.pack("!B", 0) + struct.pack("!HH", 1, 1)
-
-    # print(header, "Question is", question)
+    
     request = header + question
-    # print("Total string is", request)
+    
     return request
 
-def recieveDNSResponse(socket):
-    recieve, addr = socket.recvfrom(1024)
-    id, flags, questions, answerRR, authorityRR, additionalRR = struct.unpack("!HHHHHH", recieve[0:12])
+def recieveDNSResponse(recieve):
+    ipList = []
+    dnsTypeList = []
     
+    # ignore the header, header is 12 bytes
     recieveCounter = 12
     query = ("1")
     
@@ -41,17 +49,12 @@ def recieveDNSResponse(socket):
         recieveCounter += 1
 
     # increment bytes for type and class
-    ntype, nclass = struct.unpack("!HH", recieve[recieveCounter : recieveCounter + 4])
-    # print("Type and class are ", ntype, nclass)
     recieveCounter += 4
-
-    totalRR = answerRR + authorityRR + additionalRR
     
-    # parseNameOfNameServers is tuple, which is needed to unpack
+    # parseNameOfNameServers is tuple
     parseNameOfNameServers = ("1")
-    for answer in range(totalRR):
+    while recieveCounter < len(recieve):
         parseNameOfNameServers = struct.unpack("!B", recieve[recieveCounter : recieveCounter + 1])
-        # print((parseNameOfNameServers[0]))
         
         # Check if first two bits in string are 11 using int (11000000 == 192)
         if (parseNameOfNameServers[0] >= 192):
@@ -60,80 +63,123 @@ def recieveDNSResponse(socket):
         else:
             while(parseNameOfNameServers[0] != 0):
                 parseNameOfNameServers = struct.unpack("!B", recieve[recieveCounter : recieveCounter + 1])
-                # print("Name servers:", parseNameOfNameservers)
                 recieveCounter += 1
         
         dnsType, dnsClass = struct.unpack("!HH", recieve[recieveCounter : recieveCounter + 4])
-        # print("DNS type and class:", dnsType, dnsClass)
+        dnsTypeList.append(dnsType)
         recieveCounter += 4
         
         # time to live (TTL) is the next field, 4 bytes long but don't need
         recieveCounter += 4
         
         dnsDataLength = struct.unpack("!H", recieve[recieveCounter : recieveCounter + 2])
-        # print("Data len", dnsDataLength[0])
         recieveCounter += 2
         
         # A Type and IN Class
         if (dnsType == 1 and dnsClass == 1):
+            # each section for IP is 1 byte, so use B
             ip = struct.unpack("!BBBB", recieve[recieveCounter : recieveCounter + 4])
             ipToString = ''
             for i in range(len(ip)):
                 ipToString += str(ip[i])
                 if i != len(ip) - 1:
                     ipToString += "."
-            # print(ipToString)
-            return ipToString
+            recieveCounter += 4
+            ipList.append(ipToString)
+            
         else:
             recieveCounter += dnsDataLength[0]
-        # print("Iterator at ", answer + 1)
+            
+    return copy.deepcopy(ipList), copy.deepcopy(dnsTypeList)
 
-def send_http_request_tcp_by_ip(ip, port):
+def typesToString(typeList, name):
+    listLength = len(typeList)
+    string = f"There were {listLength} records in the {name}: \n"
+    
+    # translating list to DNS types talked about in the textbook + AAAA
+    for element in range(listLength):
+        typeName = "TYPE "
+        if typeList[element] == 1:
+            typeName += "A"
+        elif typeList[element] == 2:
+            typeName += "NS"
+        elif typeList[element] == 5:
+            typeName = "CNAME"
+        elif typeList[element] == 15:
+            typeName += "MX"
+        elif typeList[element] == 28:
+            typeName += "AAAA"
+        else:
+            typeName = ""
+        string += typeName
+        string += '\n'
+        
+    return string
+
+def storeContents(fileName, content):
+    # create a new file and store data
+    with open(fileName, "w") as response_file:
+        response_file.write(content)
+
+def sendHTTPRequest(ip, port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcpSocket:
         tcpSocket.connect((ip, port))
-        tcpSocket.settimeout(3)
-
-        request = f"GET / HTTP/1.1\r\nHost: {ip}\r\n\r\n"
+        request = "GET / HTTP/1.1\r\n"
+        request += "\r\n"
         
+        startTime = time.time()
         tcpSocket.sendall(request.encode())
 
         response = ""
-        try:
-            while True:
-                data = tcpSocket.recv(1024).decode()
-                if not data:
-                    tcpSocket.close()
-                    break
-                response += data
-        except socket.timeout:
-            pass
+        while True:
+            data = tcpSocket.recv(1024)
+            if not data:
+                endTime = time.time()
+                tcpSocket.close()
+                break
+            response += data.decode()
 
-        return response
-    
-def main():
+        elapsedTime = (endTime - startTime) * 1000
+        return response, elapsedTime
+
+def sendAndRecieve(ip, dnsServer):
     udpSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     message = PacketConstruion(WEBSITE)
-    udpSocket.sendto(message, (ROOTIP, DNSPORT))
-
-    tldIP = recieveDNSResponse(udpSocket)
-    udpSocket.sendto(message, (tldIP, DNSPORT))
-
-    authoratativeIP = recieveDNSResponse(udpSocket)
-    udpSocket.sendto(message, (authoratativeIP, DNSPORT))
-
-    ip = recieveDNSResponse(udpSocket)
-    print(WEBSITE, "IP is", ip)
-
-    response = send_http_request_tcp_by_ip(ip, 80)
     
-    print(response)
+    startTime = time.time()
+    
+    udpSocket.sendto(message, (ip, DNSPORT))
+    response, addr = udpSocket.recvfrom(1024)
+    
+    endTime = time.time()
+    
+    elapsedTime = (endTime - startTime) * 1000
+    
+    # IP is list of IP's, type is the list of DNS types from DNS Server
+    IP, Type = recieveDNSResponse(response)
+    storeContents(f"{dnsServer}ResponseTypes.txt", typesToString(Type, f"{dnsServer} DNS"))
+    
+    return IP, elapsedTime
+    
+    
+def main():
+    # returned IP's are lists
+    tldIP, rootTime = sendAndRecieve(ROOTIP, "Root")
+    authoritativeIP, tldTime = sendAndRecieve(tldIP[0], "TLD")
+    websiteIP, authoritativeTime = sendAndRecieve(authoritativeIP[0], "Authoritative")
 
-    # Uncomment to export to file
-    # with open("Response.txt", "w") as response_file:
-    #     response_file.write(response)
+    response, httpTime = sendHTTPRequest(websiteIP[0], HTTPPORT)
+    storeContents("httpRequest.txt", response)
+    
+    timeString = f"Root RTT is {str(rootTime)} ms\n"
+    timeString += f"TLD RTT is {str(tldTime)} ms\n"
+    timeString += f"Authoratative RTT is {str(authoritativeTime)} ms\n"
+    timeString += f"HTTP RTT is {str(httpTime)} ms\n"
+    storeContents("RTT.txt", timeString)
     
 if __name__ == "__main__":
     main()
+    print("Done")
     
 
 
